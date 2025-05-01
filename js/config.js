e4a9170b775006322ce4efe797ad1ad763879f41
Jason Gibson <76972956+fingergunsDEV@users.noreply.github.com>
2025-05-01 07:40:33 -0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,12 @@
+// Configuration for the different integrations
+export const config = {
+  google: {
+    clientId: 'placeholder-client-id',
+    apiKey: 'placeholder-api-key',
+    scope: 'https://www.googleapis.com/auth/drive.readonly https://www.googleapis.com/auth/spreadsheets.readonly'
+  },
+  facebook: {
+    appId: 'placeholder-app-id',
+    version: 'v12.0'
+  }
+};
